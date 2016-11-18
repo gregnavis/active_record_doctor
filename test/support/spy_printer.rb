faa23865681e542c9fad82e2024d1de93127134d
Greg Navis <contact@gregnavis.com>
2016-11-18 11:56:00 +0100

    Remove unused methods and attributes from SpyPrinter

--- test/support/spy_printer.rb
+++ test/support/spy_printer.rb
@@ -1,10 +1,9 @@
 class SpyPrinter
-  attr_reader :unindexed_foreign_keys, :extraneous_indexes, :indexed_primary_key
+  attr_reader :unindexed_foreign_keys, :extraneous_indexes
 
   def initialize
     @unindexed_foreign_keys = nil
     @extraneous_indexes = nil
-    @indexed_primary_key = nil
   end
 
   def print_unindexed_foreign_keys(argument)
@@ -20,14 +19,6 @@ class SpyPrinter
       fail("print_extraneous_indexes cannot be called twice")
     else
       @extraneous_indexes = argument
-    end
-  end
-
-  def print_indexed_primary_keys(argument)
-    if @extraneous_indexes
-      fail("indexed_primary_key cannot be called twice")
-    else
-      @indexed_primary_key = argument
     end
   end
 end
