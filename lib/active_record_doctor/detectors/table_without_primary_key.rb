945d3c8b4a542373a73de85581fede3ae0ce9158
fatkodima <fatkodima123@gmail.com>
2024-03-02 01:25:48 +0200

    Add `table_without_primary_key` checker (#173)

---
+++ lib/active_record_doctor/detectors/table_without_primary_key.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+require "active_record_doctor/detectors/base"
+
+module ActiveRecordDoctor
+  module Detectors
+    class TableWithoutPrimaryKey < Base # :nodoc:
+      @description = "detect tables without primary keys"
+      @config = {
+        ignore_tables: {
+          description: "tables whose primary key existense should not be checked",
+          global: true
+        }
+      }
+
+      private
+
+      def message(table:)
+        "add a primary key to #{table}"
+      end
+
+      def detect
+        each_table(except: config(:ignore_tables)) do |table|
+          column = primary_key(table)
+          problem!(table: table) unless column
+        end
+      end
+    end
+  end
+end
